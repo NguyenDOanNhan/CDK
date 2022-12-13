e7362ad12fc21ac14c8db9aae6ecdbe57308b45e
NguyenDOanNhan <nguyendoannhan.ptit@gmail.com>
2022-12-13 09:07:50 +0700

    NhanND Update

--- virbo/virbo/user-data.sh
+++ virbo/virbo/user-data.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 apt-get update && upgrade -y
-apt-get install -y nginx mysql-client nodejs
+apt-get install -y nginx mysql-client nodejs npm
+apt-get update
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+source ~/.bashrc
+nvm install v16.17.1
